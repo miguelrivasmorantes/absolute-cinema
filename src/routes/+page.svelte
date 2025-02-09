55987ddf5e3548561fa6300c94d48e15f9e6cf8e
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-02-10 00:23:15 +0100

    feature/change to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import 'bootstrap/dist/css/bootstrap.min.css';
+  </script>
+  
+  <div class="navbar d-flex bg-dark"> 
+    <img src="/cinema_old_icon.svg" alt="Logo" class="logo icon-fluid" />
+  
+    <div class="navbar-nav">
+      <ul>
+        <li>
+          <a class="button" href="/">Home</a>
+        </li>
+        <li>
+          <a class="button" href="/movies">Movies</a>
+        </li>
+        <li>
+          <a class="button" href="/actors">Actors</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+  <main>
+  <h1>hola</h1>
+  </main>
+  
