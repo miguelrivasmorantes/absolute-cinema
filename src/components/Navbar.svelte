0b607f382b401c37d98dc46884ea2ecddb0e5342
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-02-10 00:30:32 +0100

    fix/more changes

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<!-- src/components/Navbar.svelte -->
+<script>
+    // Puedes agregar lógica aquí si es necesario
+  </script>
+  
+  <nav class="navbar">
+    <img src="/cinema_old_icon.svg" alt="Logo" class="logo icon-fluid" />
+    <div class="navbar-nav">
+      <ul>
+        <li><a href="/" class="button">Home</a></li>
+        <li><a href="/movies" class="button">Movies</a></li>
+        <li><a href="/actors" class="button">Actors</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+  <style>
+    /* Puedes agregar estilos personalizados aquí */
+  </style>
+  
